5cbd8f761e750cde62fedee984fcc15f86253446
somnis23 <somnis0422@gmail.com>
2026-04-28 16:13:15 +0900

    Fix: 근접 콤보 및 회피 몽타주 네트워크 동기화 수정
    - 콤보 입력 처리, 섹션 점프 검증, 근접 몽타주 재생/정지 전파 로직 수정
    - 근접 공격 중 회피 입력 시 활성 근접 GA 취소 처리 추가
    - 활성 GA 입력 Pressed/Released 서버 전파 보강
    - 근접 몽타주 재생/섹션 점프/정지 멀티캐스트 구현 추가
    - 콤보 시작,종료 포인트 notify 추가

--- Source/ProjectBlackout/GAS/Abilities/Player/BlackoutGA_Dodge.cpp
+++ Source/ProjectBlackout/GAS/Abilities/Player/BlackoutGA_Dodge.cpp
@@ -4,6 +4,7 @@
 #include "Characters/BlackoutPlayerCharacter.h"
 #include "Combat/Components/BlackoutCombatComponent.h"
 #include "Animation/AnimMontage.h"
+#include "GAS/Abilities/Player/BlackoutGA_MeleePlayer.h"
 #include "GAS/BlackoutAbilitySystemComponent.h"
 #include "Core/BlackoutLog.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -46,6 +47,12 @@ void UBlackoutGA_Dodge::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
 		BO_LOG_GAS(Warning, "GA_Dodge failed: CommitAbility 실패");
 		EndAbility(Handle, ActorInfo, ActivationInfo, true, true);
 		return;
+	}
+
+	if (UBlackoutGA_MeleePlayer* MeleeAbility = UBlackoutGA_MeleePlayer::GetActiveMeleeAbilityFromActor(PlayerCharacter))
+	{
+		BO_LOG_GAS(Log, "GA_Dodge cancelling active melee before dodge");
+		MeleeAbility->K2_CancelAbility();
 	}
 	
 	// TODO : 백스텝 임시 항상 FALSE  , 추후 방향입력 X DODGE 실행시 백스텝 추가 예정 
